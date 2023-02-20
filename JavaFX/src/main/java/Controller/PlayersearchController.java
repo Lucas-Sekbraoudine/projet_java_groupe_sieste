69ca2eeeee5f20263ca6214d080c9d04d6be1c0c
Lucas <43614096+Twistere@users.noreply.github.com>
2023-02-20 15:06:37 +0100

    wip

--- JavaFX/src/main/java/Controller/PlayersearchController.java
+++ JavaFX/src/main/java/Controller/PlayersearchController.java
@@ -1,4 +1,22 @@
 package Controller;
 
+import Client.Client;
+import Common.Message;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class PlayersearchController {
+
+    Client client;
+
+    public PlayersearchController(Client client){
+        this.client = client;
+    }
+    @FXML
+    public void handlePlayerSearch(ActionEvent event) throws IOException {
+        client.sendMessage((new Message("search", "search")));
+
+    }
 }
